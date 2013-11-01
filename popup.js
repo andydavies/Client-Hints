54cd914a8f50d1a8608bd81543900a7bae502375
Ilya Grigorik <ilya@igvita.com>
2013-11-01 13:58:55 -0700

    fix missing space in menu

--- popup.js
+++ popup.js
@@ -27,7 +27,7 @@ function onClick(event) {
 function serialize(obj) {
   var description =  '';
   for (var hint in obj) {
-    description += (hint + ": " + obj[hint]);
+    description += (hint + ": " + obj[hint] + " ");
   }
 
   return description;
